40030b56f82c06e293e6a9ed093c5cbf1900a96c
Tomás <tomasleggire@hotmail.com>
2023-12-19 20:51:32 -0300

    Agregando estilos al tab navigation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Image } from "react-native";
 import { RootStackParamList } from "./NavigationTypes";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import Account from "../screens/Account";
 import Pokedex from "../screens/Pokedex";
 import Favorite from "../screens/Favorite";
@@ -10,9 +12,40 @@ const Tab = createBottomTabNavigator<RootStackParamList>();
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Favorite" component={Favorite} />
-      <Tab.Screen name="Pokedex" component={Pokedex} />
-      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{ tabBarLabel: "", tabBarIcon: () => renderPokeBall() }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Mi cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
+
+const renderPokeBall = () => {
+  return (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{ width: 72, height: 72, top: -15 }}
+    />
+  );
+};
--- src/screens/Favorite.tsx
+++ src/screens/Favorite.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
 export default function Favorite() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Favorite</Text>
-    </View>
+    </SafeAreaView>
   );
 }
