5ca759fa60cf7d9fb09de3da54aac1798ab87da2
Tomás <tomasleggire@hotmail.com>
2023-12-20 21:34:05 -0300

    consumo de api

--- src/screens/Pokedex.tsx
+++ src/screens/Pokedex.tsx
@@ -1,8 +1,17 @@
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
+import { getPokemonsApi } from "../api/pokemons";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokemonsApi()
+      .then((res) => console.log("res", res))
+      .catch((e) => console.log("err", e));
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
