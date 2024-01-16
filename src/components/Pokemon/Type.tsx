532ba2c6cac6dbf6c931ce95469bd6cc46f8c285
Tomás <tomasleggire@hotmail.com>
2024-01-16 20:14:17 -0300

    Seccion de tipos

---
+++ src/components/Pokemon/Type.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, View, Text } from "react-native";
+import { getColorByPokemonType } from "../../utils/getColorByPokemonType";
+import React from "react";
+
+interface Props {
+  types: { slot: number; type: { name: string; url: string } }[];
+}
+
+export default function Type(props: Props) {
+  const { types } = props;
+
+  return (
+    <View style={styles.content}>
+      {types.map((item, index) => (
+        <View
+          key={index}
+          style={{
+            ...styles.pill,
+            backgroundColor: getColorByPokemonType(item.type.name),
+          }}
+        >
+          <Text style={styles.name}>{item.type.name}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pill: {
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 20,
+    marginHorizontal: 10,
+  },
+  name: {
+    textTransform: "capitalize",
+  },
+});
--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Pokemon/Header";
+import Type from "../components/Pokemon/Type";
 import { ScrollView, Text } from "react-native";
 //import { SafeAreaView } from "react-native-safe-area-context";
 import { getPokemonDetailsApi } from "../api/pokemons";
@@ -41,6 +42,7 @@ export default function Pokemon(props: Props) {
         image={pokemon.sprites.other["official-artwork"].front_default}
         type={pokemon.types[0].type.name}
       />
+      <Type types={pokemon.types} />
     </ScrollView>
   );
 }
