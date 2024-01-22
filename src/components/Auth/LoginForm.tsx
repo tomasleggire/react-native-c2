64c84f69ec1efa984be926f255f95b8b942d7960
Tomás <tomasleggire@hotmail.com>
2024-01-22 20:12:07 -0300

    Formulario de login

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
 import React from "react";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm...</Text>
+      <Text style={styles.title}>Iniciar Sesion</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre de usuario"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button
+        title="Entrar"
+        onPress={() => {
+          console.log("Entrando...");
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
