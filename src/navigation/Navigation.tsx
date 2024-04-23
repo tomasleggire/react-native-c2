17badbba203ae891cbf5a3b682c98d2cd87aa8cc
Tomás <tomasleggire@hotmail.com>
2024-04-23 18:32:36 -0300

    Configurando BUILD

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -11,7 +11,7 @@ const Tab = createBottomTabNavigator<RootStackParamList>();
 
 export default function Navigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator initialRouteName="Pokedexx">
       <Tab.Screen
         name="Favorite"
         component={FavoriteNavigation}
